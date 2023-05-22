8cb91984398ec739288c0023e4da664c6a7e8493
Thor Whalen <thorwhalen1@gmail.com>
2023-05-22 18:37:17 +0200

    expert sentiment

--- likert/examples/in_their_own_words/__init__.py
+++ likert/examples/in_their_own_words/__init__.py
@@ -10,12 +10,11 @@ import pandas as pd
 import numpy as np
 
 
-
-likert_files = files('likert')
-itow_files = likert_files / 'examples' / 'in_their_own_words' 
-itow_data_files = itow_files / 'data'
+likert_files = files("likert")
+itow_files = likert_files / "examples" / "in_their_own_words"
+itow_data_files = itow_files / "data"
 # itow_survey = itow_data_files / 'in_their_own_words_survey_2021_11.xlsx'
-itow_survey = itow_data_files / 'in_their_own_words_survey_2022_01.xlsx'
+itow_survey = itow_data_files / "in_their_own_words_survey_2022_01.xlsx"
 
 
 import pandas as pd
@@ -42,7 +41,7 @@ def word_cloud(
     save_filepath=None,
     width=2538,
     height=2538,
-    background_color='black',
+    background_color="black",
     **kwargs,
 ):
     from wordcloud import WordCloud
@@ -65,7 +64,7 @@ def word_cloud(
 import re
 
 
-def extract_end_of_large_question(x, pattern=re.compile('(?<=-\ )[\-\w\ ]+$')):
+def extract_end_of_large_question(x, pattern=re.compile("(?<=-\ )[\-\w\ ]+$")):
     m = pattern.search(x)
     if m is not None:
         return m.group(0)
@@ -81,7 +80,7 @@ except (ImportError, ModuleNotFoundError) as e:
 
 
 @lru_cache
-def get_text_classifier(data_name='en-sentiment'):
+def get_text_classifier(data_name="en-sentiment"):
     return TextClassifier.load(data_name)
 
 
@@ -93,9 +92,9 @@ def _sentiment_score_object(string):
 
 def sentiment_score(string):
     score = _sentiment_score_object(string)
-    if score.value == 'NEGATIVE':
+    if score.value == "NEGATIVE":
         return -score.score
-    elif score.value == 'POSITIVE':
+    elif score.value == "POSITIVE":
         return score.score
     else:
         raise ValueError(f"Didn't know score.value could be {score.value}")
@@ -122,6 +121,17 @@ def edit_word_counts(counts, word_mapping=()):
     return c.most_common()
 
 
+def extract_dict(df: pd.DataFrame, key_col: str, val_col: str):
+    """Extracts a dict from a dataframe
+
+    >>> df = pd.DataFrame({'a': [1, 2, 3], 'b': ['X', 'Y', 'Z']})
+    >>> extract_dict(df, 'a', 'b')
+    {1: 'Z', 2: 'Y', 3: 'Z'}
+
+    """
+    return dict(zip(df[key_col], df[val_col]))
+
+
 from colour import Color
 
 ColorSpec = Union[Color, str]
@@ -130,10 +140,10 @@ HexGradients = Iterable[Hex]
 
 
 def gradients_of_hex_colors(
-    color: ColorSpec = Color('grey'), number_of_gradients=200
+    color: ColorSpec = Color("grey"), number_of_gradients=200
 ) -> HexGradients:
     color = Color(color)
-    return list(map(str, color.range_to(Color('white'), number_of_gradients + 2)))[1:-1]
+    return list(map(str, color.range_to(Color("white"), number_of_gradients + 2)))[1:-1]
 
 
 def _range_index(x, n_indices, min_x, max_x):
@@ -148,11 +158,14 @@ def mk_score_to_color(
 ):
     """Makes a function that produces a color for a given score (number)"""
     if colors is None:
-        colors = 'grey'
+        colors = "grey"
     if isinstance(colors, (str, Color)):
         colors = gradients_of_hex_colors(colors)
     score_to_index = partial(
-        _range_index, n_indices=len(colors), min_x=min_score, max_x=max_score,
+        _range_index,
+        n_indices=len(colors),
+        min_x=min_score,
+        max_x=max_score,
     )
     return lambda score: colors[score_to_index(score)]
 
@@ -197,25 +210,26 @@ def word_count_for_counts_with_color_control(
 from collections import Counter
 
 term_mapping = {
-    'addict': 'Addict',
-    'slang': 'Slang',
-    'user': 'User',
-    'other': 'Other',
+    "addict": "Addict",
+    "slang": "Slang",
+    "user": "User",
+    "other": "Other",
 }
 
 label_mapping = {
-    'others who use drugs': 'Others who use',
-    'drug counselors': 'Counselor',
-    'family': 'Family',
-    'doctors': 'Doctor',
-    '12-Step mutual support members': '12-Step',
+    "others who use drugs": "Others who use",
+    "drug counselors": "Counselor",
+    "family": "Family",
+    "doctors": "Doctor",
+    "12-Step mutual support members": "12-Step",
 }
+
 
 # @lru_cache(maxsize=1)
 def get_term_category(data):
-    term_category = data['Q14']
+    term_category = data["Q14"]
     term_category = term_category.apply(term_mapping.get)
-    term_category.name = 'label'
+    term_category.name = "label"
     return term_category
 
 
@@ -228,7 +242,7 @@ def iter_pairs(category, contexts):
 
 # @lru_cache(maxsize=1)
 def get_q2_data(data, questions):
-    q2_cols = data.columns[[c.startswith('Q2') for c in data.columns]]
+    q2_cols = data.columns[[c.startswith("Q2") for c in data.columns]]
     q2_questions = list(map(questions.__getitem__, q2_cols))
     # print(f"{q2_cols=}")
     # print(f"{q2_questions=}")
@@ -255,7 +269,7 @@ def get_term_interlocutor_data(data, questions):
 
     s = data[q2_cols]
     s.columns = context_labels
-    s = s.applymap(lambda x: {'Yes': True, 'No': False}.get(x, x))
+    s = s.applymap(lambda x: {"Yes": True, "No": False}.get(x, x))
 
     return s
 
@@ -267,7 +281,7 @@ def get_multiple_counts(data, questions):
 
     # missing = n - total_counts; missing.name = 'NA/missing'
     missing = s.isna().sum().loc[list(label_mapping.values())]
-    missing.name = 'NA/missing'
+    missing.name = "NA/missing"
 
     other_counts = Counter(iter_pairs(term_category, s))
     other_counts = pd.Series(other_counts).unstack().T
@@ -276,16 +290,16 @@ def get_multiple_counts(data, questions):
     ]
     # print(other_counts)
     term_counts = pd.Series(Counter(term_category)).loc[list(term_mapping.values())]
-    term_counts.name = 'Self'
+    term_counts.name = "Self"
 
     counts = pd.concat([term_counts, other_counts], axis=1)
     # reorder index and columns
     counts = counts.loc[list(term_mapping.values())][
-        ['Self'] + list(label_mapping.values())
+        ["Self"] + list(label_mapping.values())
     ]
 
     total_counts = counts.sum()
-    total_counts.name = 'n'
+    total_counts.name = "n"
 
     interlocutor_transparency_counts = total_counts.iloc[1:].loc[
         list(label_mapping.values())
@@ -313,11 +327,11 @@ convert_to_percentage = lambda x, n=1: round(x * 100, n)
 
 
 @np.vectorize
-def _elementwise_string_join(*elements, sep=' '):
+def _elementwise_string_join(*elements, sep=" "):
     return sep.join(map(str, elements))
 
 
-def elementwise_string_join(*elements, sep=' '):
+def elementwise_string_join(*elements, sep=" "):
     first_df, *_ = elements
     return pd.DataFrame(
         data=_elementwise_string_join(*elements, sep=sep),
@@ -349,7 +363,11 @@ from statsmodels.stats.proportion import proportion_confint
 
 
 def proportion_confint_df(
-    counts, total_counts=None, method='beta', alpha=0.05, interval_chars='()',
+    counts,
+    total_counts=None,
+    method="beta",
+    alpha=0.05,
+    interval_chars="()",
 ):
     open_char, close_char = interval_chars
     if total_counts is None:
@@ -372,7 +390,7 @@ def proportion_confint_df(
             series = pd.Series(
                 data=list(
                     map(
-                        lambda x: f'{open_char}{x[0]:.1f}-{x[1]:.1f}%{close_char}',
+                        lambda x: f"{open_char}{x[0]:.1f}-{x[1]:.1f}%{close_char}",
                         zip(lo, hi),
                     )
                 ),
@@ -389,30 +407,46 @@ def proportion_confint_df(
 
 
 def proportion_and_confint(
-    counts, total_counts=None, method='beta', alpha=0.05, interval_chars='()',
+    counts,
+    total_counts=None,
+    method="beta",
+    alpha=0.05,
+    interval_chars="()",
 ):
     if total_counts is None:
         total_counts = counts.sum()
     proportions_df = auto_divide(counts, total_counts)
     confint_df = proportion_confint_df(
-        counts, total_counts, method=method, alpha=alpha, interval_chars=interval_chars,
+        counts,
+        total_counts,
+        method=method,
+        alpha=alpha,
+        interval_chars=interval_chars,
     )
     return proportions_df, confint_df
 
 
 def proportion_with_confint(
-    counts, total_counts=None, method='beta', alpha=0.05, interval_chars='()',
+    counts,
+    total_counts=None,
+    method="beta",
+    alpha=0.05,
+    interval_chars="()",
 ):
     proportions_df, confint_df = proportion_and_confint(
-        counts, total_counts, method=method, alpha=alpha, interval_chars=interval_chars,
+        counts,
+        total_counts,
+        method=method,
+        alpha=alpha,
+        interval_chars=interval_chars,
     )
     return elementwise_string_join(
-        (proportions_df * 100).round(2).applymap(lambda x: f'{x}%'), confint_df
+        (proportions_df * 100).round(2).applymap(lambda x: f"{x}%"), confint_df
     )
 
 
 def compute_stats_on_the_difference_of_proportions(
-    p1, p2, n1, n2, alternative='two-sided', prop_var=False
+    p1, p2, n1, n2, alternative="two-sided", prop_var=False
 ):
     r"""Compute the test statistic and p-value for the difference of two proportions.
 
@@ -466,11 +500,11 @@ def compute_stats_on_the_difference_of_proportions(
 
 def compute_stats_on_the_difference_of_proportions_df(
     df: pd.DataFrame,
-    p1_col: str = 'p1',
-    p2_col: str = 'p2',
-    n1_col: str = 'n1',
-    n2_col: str = 'n2',
-    alternative='two-sided',
+    p1_col: str = "p1",
+    p2_col: str = "p2",
+    n1_col: str = "n1",
+    n2_col: str = "n2",
+    alternative="two-sided",
     prop_var=False,
 ):
     """Computes the multiple difference of proportions stats given a dataframe with
@@ -505,7 +539,7 @@ def compute_stats_on_the_difference_of_proportions_df(
     # unpack the stats into separate columns
     stats_df = pd.DataFrame(
         data=list(stats),
-        columns=['p_value', 'chi_squared', 'z_score'],
+        columns=["p_value", "chi_squared", "z_score"],
         index=df.index,
     )
     return stats_df
@@ -516,7 +550,7 @@ def compute_stats_on_the_difference_of_proportions_df(
 
 def print_shortest_and_longest(names):
     unik_names = set(names)
-    print('Smallest:')
-    print('\t' + '\n\t'.join(sorted(unik_names, key=len, reverse=False)[:5]))
-    print('\nLargest:')
-    print('\t' + '\n\t'.join(sorted(unik_names, key=len, reverse=True)[:5]))
+    print("Smallest:")
+    print("\t" + "\n\t".join(sorted(unik_names, key=len, reverse=False)[:5]))
+    print("\nLargest:")
+    print("\t" + "\n\t".join(sorted(unik_names, key=len, reverse=True)[:5]))
